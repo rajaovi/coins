7d47515f11c8b93a7d79934de7583d1887dd5420
Raja <orajacse@gmail.com>
2024-09-02 08:51:26 +0530

    Learn React Native Project

--- apps/root/frontend/mobile-app/content/App.js
+++ apps/root/frontend/mobile-app/content/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Button, FlatList, StyleSheet, View } from "react-native";
+import { StatusBar } from "react-native";
 
 import GoalItem from "./components/GoalItem";
 import Goalnput from "./components/GoalInput";
@@ -31,44 +32,50 @@ export default function App() {
   }
 
   return (
-    <View style={styles.appContainer}>
-      <Button
-        title="Add New Goal"
-        color="#5e0acc"
-        onPress={startAddGoalHandler}
-      />
-      {modalVisible && (
-        <Goalnput
-          visible={modalVisible}
-          onAddGoal={handleButton}
-          onCancel={endAddGoalHandler}
-        />
-      )}
+    <>
+      <StatusBar style="light" />
 
-      <View>
-        <FlatList
-          data={goal}
-          renderItem={(value) => {
-            return (
-              <GoalItem
-                text={value.item.text}
-                id={value.item.id}
-                onDeleteItem={deleteGoalHandler}
-              />
-            );
-          }}
-          keyExtractor={(val, ind) => {
-            return val.id;
-          }}
-          alwaysBounceVertical={false}
+      <View style={styles.appContainer}>
+        <Button
+          title="Add New Goal"
+          color="#b180f0"
+          onPress={startAddGoalHandler}
         />
+        {modalVisible && (
+          <Goalnput
+            visible={modalVisible}
+            onAddGoal={handleButton}
+            onCancel={endAddGoalHandler}
+          />
+        )}
+
+        <View>
+          <FlatList
+            data={goal}
+            renderItem={(value) => {
+              return (
+                <GoalItem
+                  text={value.item.text}
+                  id={value.item.id}
+                  onDeleteItem={deleteGoalHandler}
+                />
+              );
+            }}
+            keyExtractor={(val, ind) => {
+              return val.id;
+            }}
+            alwaysBounceVertical={false}
+          />
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   appContainer: {
+    backgroundColor: "#1e085a",
+    flex: 1,
     paddingTop: 40,
     paddingHorizontal: 15,
   },
--- apps/root/frontend/mobile-app/content/components/GoalInput.js
+++ apps/root/frontend/mobile-app/content/components/GoalInput.js
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { StyleSheet, View, Button, TextInput, Modal } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Button,
+  TextInput,
+  Modal,
+  Image,
+} from "react-native";
 
 function Goalnput(props) {
   const [name, setName] = useState("");
@@ -16,6 +23,10 @@ function Goalnput(props) {
   return (
     <Modal visible={props.visible} animationType="slide">
       <View style={styles.inputContainer}>
+        <Image
+          source={require("../assets/images/goal.png")}
+          style={styles.image}
+        />
         <TextInput
           style={styles.textInput}
           placeholder="Enter Input"
@@ -24,10 +35,10 @@ function Goalnput(props) {
         />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title="Add Button" onPress={handleGoal} />
+            <Button title="Add Button" onPress={handleGoal} color="#b180f0" />
           </View>
           <View style={styles.button}>
-            <Button title="Cancel" onPress={props.onCancel} />
+            <Button title="Cancel" onPress={props.onCancel} color="#f31282" />
           </View>
         </View>
       </View>
@@ -39,18 +50,25 @@ export default Goalnput;
 
 const styles = StyleSheet.create({
   inputContainer: {
+    backgroundColor: "#311b6b",
     flex: 1,
-    borderBottomWidth: 2,
-    borderBottomColor: "#ccc",
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 20,
     padding: 20,
   },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
+  },
   textInput: {
+    backgroundColor: "#e4d0ff",
     borderWidth: 1,
-    borderColor: "#333",
+    borderColor: "#e4d0ff",
+    color: "#120438",
     padding: 8,
+    borderRadius: 6,
+    padding: 16,
     width: "100%",
   },
   buttonContainer: {
